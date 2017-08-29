3eee524556e4a0e07883ee4ce71312b390f3b6b0
sonalsatpute <sonal.satpute@gmail.com>
2017-08-29 16:00:59 +0530

    added sample to test dev environment

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var config = {
+    entry: './main.js',
+     
+    output: {
+       path:'/',
+       filename: 'bundle.js',
+    },
+     
+    devServer: {
+       inline: true,
+       port: 8087
+    },
+     
+    module: {
+       loaders: [
+          {
+             test: /\.jsx?$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader',
+                 
+             query: {
+                presets: ['es2015', 'react']
+             }
+          }
+       ]
+    }
+ }
+ 
+ module.exports = config;
